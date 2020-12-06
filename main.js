4e99f6b7d50c338e49f3ece2f24e2e51696821aa
jrdntnnr <jordanmtanner@gmail.com>
2020-12-06 17:06:49 +0200

    Fix AR

---
+++ main.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const scene = document.querySelector('a-scene');
+    const splash = document.querySelector('#splash');
+    const loading = document.querySelector('#splash .loading');
+    const startButton = document.querySelector('#splash .start-button');
+
+    const emitEvent = (eventName, listeners) => {
+        listeners.forEach((listener) => {
+            const el = document.querySelector(listener);
+            el.emit(eventName);
+        })
+    };
+
+    const emitMediaEvent = (eventType, listeners) => {
+        listeners.forEach((listener) => {
+            const el = document.querySelector(listener);
+            el.components.sound[eventType]();
+        })
+    };
+
+    const activateSoundsForTouch = () => {
+    	const sounds = document.querySelectorAll('[sound]')
+        sounds.forEach((soundEl) => {
+            soundEl.components.sound.playSound();
+            soundEl.components.sound.stopSound();
+        })
+    };
+
+
+
+    scene.addEventListener('loaded', function (e) {
+        setTimeout(() => {
+            loading.style.display = 'none';
+            splash.style.display = 'none';
+            startButton.style.opacity = 1;
+        }, 50);
+    });
+
+    startButton.addEventListener('click', function (e) {
+        activateSoundsForTouch();
+        splash.style.display = 'none';
+        emitEvent('scene-started', ['[sound]']);
+    });
+});
